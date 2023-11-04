e40cb608ee82c790db75637b66e13630d73efb6d
h1t3re <143657823+h1t3re@users.noreply.github.com>
2023-11-04 11:22:40 +0100

    Add files via upload

--- strcontains.c
+++ strcontains.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include "../../include/strlen.h"
-#include <stdio.h>
+#include "../../include/strdup.h"
 
 int strcontains(const char *str1, const char *str2)
 {
@@ -10,7 +10,9 @@ int strcontains(const char *str1, const char *str2)
 	int strlen_array2 = 0;
 	char *array1;
 	char *array2;
-	
+	char *tmp;
+	int tmp0 = 0;
+
 	while(strlen_array1 < strlen(str1))
 		strlen_array1 = strlen_array1 +1;
 	while(strlen_array2 < strlen(str2))
@@ -30,29 +32,24 @@ int strcontains(const char *str1, const char *str2)
 		*(array2 + i)	= *(str2 + i);
 		i = i +1;
 	}
+	if(strlen_array1 < strlen_array2)
+	{
+		tmp = strdup(array1);
+		array1 = strdup(array2);
+		array2 = tmp;
+		tmp0 = strlen_array1;
+		strlen_array1 = strlen_array2;
+		strlen_array2 = tmp0;
+	}
 	i = 0;
-	if(strlen_array1 > strlen_array2)
+	while(*(array1 + i) != '\0')
 	{
-		while(*(array1 + i) != '\0')
-		{
-			j = 0;
-			while((*(array1 + i + j) == (*(array2 + j))) & (*(array2 + j) != '\0'))
-				j = j + 1;
-			if(j == strlen_array2)
-				return 0;
-			i = i +1;
-		}
-	}else if(strlen_array1 < strlen_array2)
-	{
-		while(*(array2 + i) != '\0')
-		{
-			j = 0;
-			while((*(array2 + i + j) == (*(array1 + j))) & (*(array1 + j) != '\0'))
-				j = j + 1;
-			if(j == strlen_array1)
-				return 0;
-			i = i +1;
-		}
+		j = 0;
+		while((*(array1 + i + j) == (*(array2 + j))) & (*(array2 + j) != '\0'))
+			j = j + 1;
+		if(j == strlen_array2)
+			return 0;
+		i = i +1;
 	}
 	return 1;
 }
